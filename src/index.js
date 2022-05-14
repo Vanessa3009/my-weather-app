d76cb7dfe76ef4ad301134b69a0a465c41421226
theoneandonlyV <vanessa.dressler@gmx.de>
2022-05-14 17:24:14 +0200

    adapted icon

--- src/index.js
+++ src/index.js
@@ -35,6 +35,12 @@ function displayWeather(response) {
   );
   document.querySelector("#condition").innerHTML =
     response.data.weather[0].main;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
 function searchCity(city) {
